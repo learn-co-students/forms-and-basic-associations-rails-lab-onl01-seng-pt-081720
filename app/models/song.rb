bbbf7ff3e6c41dbb0aa770c60f711404f58985a1
Brenda Ferrufino <ferrufinob@gmail.com>
2020-12-27 15:39:01 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,37 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  #sets the artist to the artist specified name, instead of an ID
+  # this method is called whenever SOng is initialized with a artist_name field
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  #returns the artist name
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  #sets genre
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  #returns the genre name
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  #iterate through note an "build it"
+  def song_notes=(notes)
+    notes.each do |note|
+      self.notes.build(content: note)
+    end
+  end
+
+  def song_notes
+    self.notes.map(&:content)
+  end
 end
