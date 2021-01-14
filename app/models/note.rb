708f3e0377f0f87ef9d0fb84075f0a0dfdec8c16
jasonemarkson <jasonmarkson76@gmail.com>
2021-01-13 20:00:15 -0500

    Done.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,11 @@
 class Note < ActiveRecord::Base
   # add associations here
   belongs_to :song
+
+  def song_ids=(ids)
+    ids.each do |id|
+      note = Song.find(id)
+      self.notes << note
+    end
+  end
 end
