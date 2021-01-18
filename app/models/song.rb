609355da75dad54117d77140a7b95c8e1c533315
scarter31191 <scarter31191@gmaile.com>
2021-01-17 21:38:57 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,19 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-  
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      self.notes.build(content: content)
+    end
+  end
+
 end
